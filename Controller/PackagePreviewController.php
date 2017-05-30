891ebd455e4802261dfac12260b3ac80bb3fd5dc
Rafał Muszyński <rmuszynski1@gmail.com>
2017-05-30 13:36:29 +0200

    ElasticSearch integration & bugfixes (#336)
    * elasticsearch bundle
    
    * fixes
    
    * search controller fix
    
    * docs
    
    * added more filters
    
    * filters doc
    
    * app kernel
    
    * composer json
    
    * include lock
    
    * fixes
    
    * improvements
    
    * params
    
    * improvements
    
    * removed dump
    
    * fix
    
    * temp
    
    * docs
    
    * add elasticsearch to travis, spec fix
    
    * added command
    
    * fixes
    
    * organization and single content rules fix, fixes
    
    * On published article if change route and publish article, route isn't changed
    
    * spec fix
    
    * Add fbia parameter to articles in packages
    
    * doc block
    
    * mapping fix
    
    * In packages, total number of articles is not showing fine
    
    * cs fix
    
    * fix tests
    
    * tests fix
    
    * fix spec
    
    * travis update
    
    * elasticsearch 5.4 - travis
    
    * travis elasticsearch
    
    * travis fix
    
    * test config
    
    * es index aliases
    
    * travis fix
    
    * fix
    
    * tests fix
    
    * added metadata search
    
    * api doc
    
    * travis
    
    * logs
    
    * travis
    
    * elasticsearch travis
    
    * travis config
    
    * travis fix
    
    * travis elastic
    
    * travis env test
    
    * fix
    
    * fix
    
    * config
    
    * fix
    
    * fix
    
    * test
    
    * test
    
    * test
    
    * test
    
    * travis
    
    * swap
    
    * swap
    
    * increase swap
    
    * tests fix
    
    * travis swap increase
    
    * final fixes
    
    * fix
    
    * fix
    
    * update package on publish
    
    * fixes re PR comments
    
    * search result loader
    
    * search result loader tests
    
    * media loder fix
    
    * set http cache version

---
+++ Controller/PackagePreviewController.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Superdesk Web Publisher Content Bundle.
+ *
+ * Copyright 2017 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2017 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Bundle\CoreBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use SWP\Bundle\ContentBundle\Model\RouteInterface;
+use SWP\Bundle\CoreBundle\Model\PackageInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PackagePreviewController extends Controller
+{
+    /**
+     * @Route("/preview/package/{routeId}/{id}", options={"expose"=true}, requirements={"slug"=".+", "routeId"="\d+", "token"=".+"}, name="swp_package_preview")
+     * @Method("GET")
+     */
+    public function previewAction(int $routeId, $id)
+    {
+        /** @var RouteInterface $route */
+        $route = $this->findRouteOr404($routeId);
+        /** @var PackageInterface $package */
+        $package = $this->findPackageOr404($id);
+        $article = $this->get('swp.factory.article')->createFromPackage($package);
+        $metaFactory = $this->get('swp_template_engine_context.factory.meta_factory');
+        $templateEngineContext = $this->get('swp_template_engine_context');
+        $templateEngineContext->setCurrentPage($metaFactory->create($route));
+        $templateEngineContext->getMetaForValue($article);
+
+        if (null === $route->getArticlesTemplateName()) {
+            throw $this->createNotFoundException(
+                sprintf('Template for route with id "%d" (%s) not found!', $route->getId(), $route->getName())
+            );
+        }
+
+        return $this->render($route->getArticlesTemplateName());
+    }
+
+    private function findRouteOr404(int $id)
+    {
+        if (null === ($route = $this->get('swp.repository.route')->findOneBy(['id' => $id]))) {
+            throw $this->createNotFoundException(sprintf('Route with id: "%s" not found!', $id));
+        }
+
+        return $route;
+    }
+
+    private function findPackageOr404(string $id)
+    {
+        if (null === ($package = $this->get('swp.repository.package')->findOneBy(['id' => $id]))) {
+            throw $this->createNotFoundException(sprintf('Package with id: "%s" not found!', $id));
+        }
+
+        return $package;
+    }
+}
